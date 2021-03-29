054475bf01eba5fe04882e75f1990bb768c0e4e0
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-03-29 14:00:43 +0500

    add: ex4: string literals

---
+++ JS-basics/objects/blog-post.js
@@ -0,0 +1,60 @@
+let post = {
+    title:'Pride and Prejudice',
+
+    body: `“You must learn some of my philosophy. Think only of the past as its 
+    remembrance gives you pleasure.”
+    
+    “He is a gentleman, and I am a gentleman's daughter. So far we are equal.”
+    
+    “I declare after all there is no enjoyment like reading! How much sooner 
+    one tires of any thing than of a book! -- When I have a house of my own, 
+    I shall be miserable if I have not an excellent library.” 
+    
+    “A lady's imagination is very rapid; it jumps from admiration to love, from
+    love to matrimony in a moment.”
+     
+    These are some quotes from my favourite book, oh, and there is another one too,
+    which is called "Testament of Youth". I love books so much>>>>`,
+
+    views: 3000,
+    comments:[
+        {author: 'Bella', body:'Wow, awesome'},
+        {author: 'Hamid', body: "Well, I don't like books of this type very much"}
+    ],
+
+    author: 'Jane Austen (1775-1815)',
+    isLive: false
+}
+
+
+var blog = `
+                        Pride and Prejudice
+
+“You must learn some of my philosophy. Think only of the past as its 
+remembrance gives you pleasure.”
+
+“He is a gentleman, and I am a gentleman's daughter. So far we are equal.”
+
+“I declare after all there is no enjoyment like reading! How much sooner 
+one tires of any thing than of a book! -- When I have a house of my own, 
+I shall be miserable if I have not an excellent library.” 
+
+“A lady's imagination is very rapid; it jumps from admiration to love, from
+ love to matrimony in a moment.”
+ 
+ These are some quotes from my favourite book, oh, and there is another one too,
+ which is called "Testament of Youth". I love books so much>>>>
+
+ Views: 3000
+
+ Comments:
+        Bella: "Wow, awesome"
+
+        Hamid: "Well, I don't like books of this type very much"
+
+Author: Jane Austen (1775-1815)
+IsLive: The author is not alive
+ `
+
+console.log(blog)
+console.log(post)
