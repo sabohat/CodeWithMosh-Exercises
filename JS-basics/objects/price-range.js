436200348754df6204c7810d5d8c27e5de97dfa9
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-03-29 14:43:49 +0500

    add: price-range with objects

---
+++ JS-basics/objects/price-range.js
@@ -0,0 +1,6 @@
+
+let priceRange =[
+    {label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10},
+    {label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20},
+    {label: '$$$', tooltip: 'Pricey', minPerPerson: 21, maxPerPerson: 50}
+]
