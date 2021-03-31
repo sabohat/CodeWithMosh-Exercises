5bfc11ec622fdcff3f0394af95ed8cb28470782f
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-03-31 13:22:16 +0500

    ex8: sort, filter, map, join and reverse methods in use

---
+++ JS-basics/arrays/movies.js
@@ -0,0 +1,21 @@
+const movies = [
+    { title: 'a', year: 2018, rating: 4.5 },
+    { title: 'b', year: 2018, rating: 4.7 },
+    { title: 'c', year: 2018, rating: 3 },
+    { title: 'd', year: 2017, rating: 4.5 },
+]
+
+// All movies in 2018, with rating > 4
+// Sort them by their rating
+// Decending order
+// Pick their title
+
+//End result : 'b', 'a'
+
+const n = movies
+                .filter(movie => movie.year === 2018 && movie.rating > 4)
+                .sort((a, b) => a.rating - b.rating)//very smart implementation
+                .reverse()
+                .map(movie => movie.title)
+                .join(', ')
+console.log(n)
