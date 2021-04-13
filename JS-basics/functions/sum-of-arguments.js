13394918dc70f904e89beb9dff5f91b01229db4b
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-04-13 15:58:59 +0500

    update: add another solution of calculating array numbers

--- JS-basics/functions/sum-of-arguments.js
+++ JS-basics/functions/sum-of-arguments.js
@@ -7,13 +7,24 @@
  function sum(...args) {
   const sum = args.reduce((a, b) => a + b)
    console.log(sum)
+   return sum
  }
 
  // Sum of numbers inside an array
-function sumOfArrayItems(array) { // no need for REST operator
-  const sum = array.reduce((a, b) => a + b)
+function sumOfArrayItems(arr) { // no need for REST operator
+  const sum = arr.reduce((a, b) => a + b)
   console.log(sum)
+  return sum
+}
+// Second method for finding the sum of array of values
+function sumOfArrayValues(...arr) { // no need for REST operator
+  if (arr.length === 1 && Array.isArray(arr[0]))
+    arr = [...arr[0]] // Spread operator
+  const sum = arr.reduce((a, b) => a + b)
+  console.log(sum)
+  return sum
 }
 
 sumOfArrayItems([6, 7, 4, 2, 8, 0])
 sum(2, 3, 4, 5, 5, 6, 7)
+sumOfArrayValues([6, 7, 4, 2, 8, 0])
