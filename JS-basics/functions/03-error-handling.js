81d0f58c71aae030111caf4b8f8a9ae9e9b1b4bb
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-04-13 16:24:34 +0500

    add: solution to 03-error-handling

---
+++ JS-basics/functions/03-error-handling.js
@@ -0,0 +1,22 @@
+
+
+try {
+  const numbers = [1, 2, 3, 4];
+  const count = countOccurrences(null, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message)
+}
+
+
+
+function countOccurrences(array, searchElement) {
+  if (! Array.isArray(array))
+    throw new Error('Invalid input')
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = (current === searchElement) ? 1 : 0;
+    console.log(accumulator, current, searchElement);
+    return accumulator + occurrence;
+  }, 0);
+}
